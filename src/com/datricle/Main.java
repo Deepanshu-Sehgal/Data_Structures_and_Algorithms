ff63ce0059dc7105f5f3d5c7f4bc3e18e9676f26
Deepanshu <deepanshusehgal.112@gmail.com>
2022-04-09 12:47:25 +0530

    Initial commit

---
+++ src/com/datricle/Main.java
@@ -0,0 +1,13 @@
+package com.datricle;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your name");
+        String name = input.next();
+        System.out.println("Hey "+name);
+    }
+}
