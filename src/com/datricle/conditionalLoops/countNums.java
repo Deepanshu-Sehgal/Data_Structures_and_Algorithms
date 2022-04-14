9cb2244945cebf4d2b44be29f4f8b02b97d144c5
Deepanshu <deepanshusehgal.112@gmail.com>
2022-04-14 15:57:27 +0530

    Conditionals

---
+++ src/com/datricle/conditionalLoops/countNums.java
@@ -0,0 +1,16 @@
+package com.datricle.conditionalLoops;
+
+public class countNums {
+    public static void main(String[] args) {
+        int n = 455356;
+        int count = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == 5) {
+                count++;
+            }
+            n = n / 10;
+        }
+        System.out.println(count);
+    }
+}
