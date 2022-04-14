e2145f96b8e327cc5cbbd6e4d22d80dba7b55bfa
Deepanshu <deepanshusehgal.112@gmail.com>
2022-04-14 15:25:22 +0530

    Conditionals

--- src/com/datricle/conditionalLoops/CaseCheck.java
+++ src/com/datricle/conditionalLoops/CaseCheck.java
@@ -6,7 +6,10 @@ public class CaseCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().trim().charAt(0);
-        if (ch >= 'a')
-        System.out.println(ch);
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lower case");
+        } else {
+            System.out.println("Upper case");
+        }
     }
 }
