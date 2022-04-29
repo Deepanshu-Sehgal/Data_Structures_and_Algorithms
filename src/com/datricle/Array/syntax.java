012303cea93900969c848ab4d0ee5b952e6f15d4
Deepanshu <deepanshusehgal.112@gmail.com>
2022-04-29 14:35:03 +0530

    Array started

---
+++ src/com/datricle/Array/syntax.java
@@ -0,0 +1,19 @@
+package com.datricle.Array;
+
+public class syntax {
+    public static void main(String[] args) {
+        /* int[] rollNo = new int[5];
+        int[] rollNo2 = {23, 34, 56, 67, 78};*/
+
+        int[] ros; //declaration of array. ros is getting defined the stack
+        ros = new int[5]; // initialisation: actually here object is being created in the heap memory
+
+        System.out.println(ros[1]);
+
+        String[] arr = new String[4];
+
+        System.out.println(arr[0]);
+
+
+    }
+}
